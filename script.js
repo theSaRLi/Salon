9626eea4287517e19cddd6a52f741ae77302d3bc
Sara <fischersara136@gmail.com>
2020-02-25 16:38:16 +0100

    animation_on_button

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function(){
     mainNav.classList.toggle('active');
 });
+
+
+let navBar = document.getElementById('js-navbar-toggle'); 
+
+navBar.addEventListener('click', function(){
+    navBar.classList.toggle('open');
+});
